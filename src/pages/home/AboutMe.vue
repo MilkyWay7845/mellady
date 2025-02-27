84cae82304e7deae4b71e6a8e88f054abd12434d
ekorobeynikov <evgenii.kurenkov@mail.ru>
2025-02-27 21:19:33 +0300

    Addaptive text

--- src/pages/home/AboutMe.vue
+++ src/pages/home/AboutMe.vue
@@ -4,9 +4,9 @@
             <img src="/2.jpg" />
             <p class="text-white font-exo2 text-md">Talya Melanskaya</p>
         </div>
-        <div class="flex flex-col w-full lg:w-2/3 text-center px-15 py-10 gap-4">
+        <div class="flex flex-col w-full lg:w-2/3 text-center sm:px-15 py-10 gap-4">
             <p class="text-white font-exo2 text-2xl">ABOUT ME</p>
-            <p class="text-white font-exo2 text-lg">Hello everyone and welcome to the world of my creativity. I love
+            <p class="text-white font-exo2 sm:text-lg">Hello everyone and welcome to the world of my creativity. I love
                 discovering new things and capturing the aesthetics of my life in my photographs. I'm not selling
                 anything, I just want to give you some creative ideas and share my work. My album is a reflection of my
                 life. Immerse yourself in my aesthetic.</p>
